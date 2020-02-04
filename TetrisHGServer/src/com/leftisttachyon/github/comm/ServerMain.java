7d80750b269e6716545c014fcd11e1a0d3dd9d0d
Gui Guy <jedandeganwang1999@gmail.com>
2020-02-04 14:11:00 -0600

    IntelliJ isn't vibing with this project structure

--- TetrisHGServer/src/com/leftisttachyon/github/comm/ServerMain.java
+++ TetrisHGServer/src/com/leftisttachyon/github/comm/ServerMain.java
@@ -1,11 +1,10 @@
 package com.leftisttachyon.github.comm;
 
+import javax.swing.*;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import javax.swing.JOptionPane;
 
 /**
  * The main class; entry point of the application
@@ -26,22 +25,22 @@ public class ServerMain {
         
         /*MainWindow mw = new MainWindow();
         Handler.setMainWindow(mw);*/
-        
-        try(ServerSocket listener = new ServerSocket(9001)) {
-            while(true) {
+
+        try (ServerSocket listener = new ServerSocket(9001)) {
+            while (true) {
                 Socket socket = listener.accept();
-                
+
                 MiniServer h = new MiniServer(socket);
-                
+
                 new Thread(h::go).start();
-                
-                PrintWriter out = new PrintWriter(socket.getOutputStream());
-                out.println("egg");
+
+//                PrintWriter out = new PrintWriter(socket.getOutputStream());
+//                out.println("egg");
                 // mw.addHandler(h);
             }
-        } catch(BindException be) {
+        } catch (BindException be) {
             System.err.println("Cannot start server: " + be.getMessage());
-            JOptionPane.showMessageDialog(null, "Cannot start server", 
+            JOptionPane.showMessageDialog(null, "Cannot start server",
                     be.getMessage(), JOptionPane.ERROR_MESSAGE);
             System.exit(1);
         }
